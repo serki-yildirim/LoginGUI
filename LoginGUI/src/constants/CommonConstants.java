d27cb440ed8bb7a95210ea0ace2df2a152b98822
serki-yildirim <159115357+serki-yildirim@users.noreply.github.com>
2024-02-06 09:00:20 +0100

    Per Drag and Drop
    Rübergezogen per drag and drop

---
+++ LoginGUI/src/constants/CommonConstants.java
@@ -0,0 +1,7 @@
+package constants;
+
+public class CommonConstants {
+    public final static String APP_NAME = "Login App";
+    public final static int[] FRAME_SIZE = {400,400};
+    public static final int TEXT_FIELD_SIZE = 15;
+}
